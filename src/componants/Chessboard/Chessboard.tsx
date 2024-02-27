9c3854fe96f31ee1e2787024f9adbf562babd310
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-02-27 21:16:50 +0530

    adding initial state of board

--- src/componants/Chessboard/Chessboard.tsx
+++ src/componants/Chessboard/Chessboard.tsx
@@ -12,7 +12,36 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
-pieces.push({ image: "assets/images/Chess_pdt60.png", x: 0, y: 6 });
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: "assets/images/Chess_pdt60.png", x: i, y: 6 });
+}
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: "assets/images/Chess_plt60.png", x: i, y: 1 });
+}
+
+//Rooks
+pieces.push({ image: "assets/images/Chess_rdt60.png", x: 0, y: 7 });
+pieces.push({ image: "assets/images/Chess_rdt60.png", x: 7, y: 7 });
+pieces.push({ image: "assets/images/Chess_rlt60.png", x: 0, y: 0 });
+pieces.push({ image: "assets/images/Chess_rlt60.png", x: 7, y: 0 });
+
+//knights
+pieces.push({ image: "assets/images/Chess_ndt60.png", x: 1, y: 7 });
+pieces.push({ image: "assets/images/Chess_ndt60.png", x: 6, y: 7 });
+pieces.push({ image: "assets/images/Chess_nlt60.png", x: 1, y: 0 });
+pieces.push({ image: "assets/images/Chess_nlt60.png", x: 6, y: 0 });
+
+//Bishops
+pieces.push({ image: "assets/images/Chess_bdt60.png", x: 2, y: 7 });
+pieces.push({ image: "assets/images/Chess_bdt60.png", x: 5, y: 7 });
+pieces.push({ image: "assets/images/Chess_blt60.png", x: 2, y: 0 });
+pieces.push({ image: "assets/images/Chess_blt60.png", x: 5, y: 0 });
+
+//King and Queen both black and white
+pieces.push({ image: "assets/images/Chess_kdt60.png", x: 4, y: 7 });
+pieces.push({ image: "assets/images/Chess_klt60.png", x: 4, y: 0 });
+pieces.push({ image: "assets/images/Chess_qdt60.png", x: 3, y: 7 });
+pieces.push({ image: "assets/images/Chess_qlt60.png", x: 3, y: 0 });
 
 export default function chessboard() {
   let board = [];
