b9160eaea4a2741e964ff74a9be3efa67a8b0bfc
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-03-01 16:32:20 +0530

    correct all basic pawn movemnets for both black and white pieces

--- src/componants/Chessboard/Chessboard.tsx
+++ src/componants/Chessboard/Chessboard.tsx
@@ -6,7 +6,7 @@ import Referee from "../../referee/Referee";
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-interface Piece {
+export interface Piece {
   image: string;
   x: number;
   y: number;
@@ -194,6 +194,7 @@ export default function Chessboard() {
               y,
               p.type,
               p.team,
+              value,
             );
 
             if (validMove) {
--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,6 +1,20 @@
-import { PieceType, TeamType } from "../componants/Chessboard/Chessboard";
+import {
+  Piece,
+  PieceType,
+  TeamType,
+} from "../componants/Chessboard/Chessboard";
 
 export default class Referee {
+  tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+    const piece = boardState.find((p) => p.x === x && p.y === y);
+
+    if (piece) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   isValidMove(
     px: number,
     py: number,
@@ -8,30 +22,26 @@ export default class Referee {
     y: number,
     type: PieceType,
     team: TeamType,
+    boardState: Piece[],
   ) {
     if (type === PieceType.PAWN) {
-      if (team === TeamType.OUR) {
-        if (py === 1) {
-          if (px === x && (y - py === 1 || y - py === 2)) {
-            return true;
-          }
-        } else {
-          if (px === x && y - py === 1) {
-            return true;
-          }
+      const specialRow = team === TeamType.OUR ? 1 : 6;
+      const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+      if (px === x && py === specialRow && y - py === 2 * pawnDirection) {
+        if (
+          !this.tileIsOccupied(x, y, boardState) &&
+          !this.tileIsOccupied(x, y - pawnDirection, boardState)
+        ) {
+          return true;
         }
-      } else {
-        if (py == 6) {
-          if (px == x && (py - y === 1 || py - y === 2)) {
-            return true;
-          }
-        } else {
-          if (px === x && py - y === 1) {
-            return true;
-          }
+      } else if (px === x && y - py === pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState)) {
+          return true;
         }
       }
     }
+
     return false;
   }
 }
